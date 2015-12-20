5e4ab36fd372638c37e7ce23d16797a2ab9ef59e
lishaojei <404923120@qq.com>
2015-12-20 19:59:35 +0800

    dddddd

--- src/com/atguigu/student/Testman.java
+++ src/com/atguigu/student/Testman.java
@@ -5,6 +5,7 @@ public class Testman {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("1111111");
+        System.out.println("22222");
 	}
 
 }
