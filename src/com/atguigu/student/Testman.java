f749b9bc70f5717556302314e1a9affbdfd4b4e6
lishaojei <404923120@qq.com>
2015-12-20 19:56:22 +0800

    secontddd

---
+++ src/com/atguigu/student/Testman.java
@@ -0,0 +1,10 @@
+package com.atguigu.student;
+
+public class Testman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("1111111");
+	}
+
+}
